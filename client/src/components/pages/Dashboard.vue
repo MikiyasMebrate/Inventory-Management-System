c963d754b4a0fae186102dc407fa2fc4b65e0bae
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-12-03 06:36:03 +0300

    dashboard card component added

--- client/src/components/pages/Dashboard.vue
+++ client/src/components/pages/Dashboard.vue
@@ -1,3 +1,35 @@
 <template>
-    <h1>Dashboard</h1>
+    <section class="grid grid-cols-1  gap-4 mb-4  md:grid-cols-2 lg:grid-cols-4 ">
+        <DashboardCard v-for="item in dashboards" :name="item.name" :icon="item.icon" />
+    </section>
 </template>
+
+<script setup>
+import DashboardCard from '../ui/DashboardCard.vue';
+import { ClipboardIcon,ArchiveBoxIcon, BuildingStorefrontIcon  ,UserIcon } from '@heroicons/vue/24/solid'
+
+const dashboards = [
+    {
+        name: 'Total Category',
+        icon: ClipboardIcon,
+        number : 10
+    },
+    {
+        name: 'Total Product',
+        icon: ArchiveBoxIcon,
+        number : 342
+    },
+    {
+        name: 'Product in Stock',
+        icon: BuildingStorefrontIcon,
+        number : 198
+    },
+    {
+        name: 'Total Users',
+        icon: UserIcon,
+        number : 3
+    },
+]
+
+
+</script>
---
+++ client/src/components/ui/DashboardCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="block md:max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+        <div class="flex flex-row justify-between gap-2">
+            <div class="w-100">
+                <!--Name-->
+                <h5 class="mb-2 text-xl  tracking-tight text-gray-400">{{name}}</h5>
+                <!--Number-->
+                <h1 class=" text-3xl font-bold">{{ number }}</h1>
+            </div>
+            <div class="w-100">
+                
+                <div class="bg-indigo-900 p-4  rounded  flex items-center justify-center text-white">
+                    <!--Icon-->
+                    <component  :is="icon" class="h-9 w-9" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const { icon, name, number } = defineProps(['icon', 'name', 'number'])
+
+</script>
