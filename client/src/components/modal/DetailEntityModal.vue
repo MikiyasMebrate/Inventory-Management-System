bfef502c6ce59fe73cfa08472eea060d13c919a1
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-12-03 16:55:13 +0300

    description removed from category table

---
+++ client/src/components/modal/DetailEntityModal.vue
@@ -0,0 +1,49 @@
+<template>
+    <fwb-modal v-if="isShowModal" @close="emitClose"
+        class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+
+        <template #header>
+            <div tabindex="-1" aria-hidden="true" class="flex items-center text-lg ">
+                {{ title }}
+            </div>
+        </template>
+
+        <template #body>
+            <div class="mb-2" v-for="(value, key) in detail" :key="key">
+                <div class="font-bold mb-2 capitalize">{{ key }}</div>
+                <div>{{ value }}</div>
+            </div>
+
+        </template>
+        <template #footer>
+            <div class="flex justify-end">
+                <fwb-button @click="emitClose" color="blue">
+                    close
+                </fwb-button>
+            </div>
+        </template>
+    </fwb-modal>
+</template>
+
+
+<script setup>
+import { FwbButton, FwbModal } from 'flowbite-vue'
+
+
+const emit = defineEmits(['close']);
+
+const props = defineProps({
+    title: String,
+    isShowModal: Boolean,
+    entityType: String,
+    detail: Object
+});
+
+
+
+
+function emitClose() {
+    emit('close');
+}
+
+</script>
--- client/src/pages/Categories.vue
+++ client/src/pages/Categories.vue
@@ -7,12 +7,16 @@
 
         <!--Add category button-->
         <div class="flex justify-end my-4">
-            <Button @click="showAddModal" title="Add Category"></Button>
+            <Button @click="toggleModal('isShowAddModal', !modalOptions.isShowAddModal)" title="Add Category"></Button>
         </div>
 
         <!--Add Modal-->
-        <AddEntityModal title="Add Category" :isShowAddModal="modalOptions.isShowAddModal" @close="closeAddModal"
-            entityType="category" v-model="formData" />
+        <AddEntityModal title="Add Category" :isShowAddModal="modalOptions.isShowAddModal"
+            @close="toggleModal('isShowAddModal', !modalOptions.isShowAddModal)" entityType="category" v-model="formData" />
+
+        <!--Detail Modal-->
+        <DetailEntityModal title="Category detail" :detail="selected" :isShowModal="modalOptions.isShowDetailModal"
+            @close="toggleModal('isShowDetailModal', !modalOptions.isShowDetailModal)" />
 
         <!--Page length option -->
         <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
@@ -35,14 +39,12 @@
                         <ArrowsUpDownIcon class="h-5 w-5 text-gray-400 hover:text-gray-600" />
                     </div>
                 </fwb-table-head-cell>
-                <fwb-table-head-cell>Description</fwb-table-head-cell>
                 <fwb-table-head-cell>
                     <div class="flex justify-between">
                         <p>Products</p>
                         <ArrowsUpDownIcon class="h-5 w-5 text-gray-400 hover:text-gray-600" />
                     </div>
                 </fwb-table-head-cell>
-                <fwb-table-head-cell>Action</fwb-table-head-cell>
                 <fwb-table-head-cell>
                     <span class="sr-only">Edit</span>
                 </fwb-table-head-cell>
@@ -52,12 +54,11 @@
                 <fwb-table-row v-for="i in 10">
                     <fwb-table-cell>{{ i }}</fwb-table-cell>
                     <fwb-table-cell>Apple MacBook Pro 17</fwb-table-cell>
-                    <fwb-table-cell>Sliver</fwb-table-cell>
-                    <fwb-table-cell>Laptop</fwb-table-cell>
-                    <fwb-table-cell>$2999</fwb-table-cell>
+                    <fwb-table-cell>99</fwb-table-cell>
                     <fwb-table-cell>
                         <div class="flex ">
-                            <EyeIcon class="h-5 w-5 text-gray-400 hover:text-gray-600" />
+                            <EyeIcon @click="toggleModal('isShowDetailModal', !modalOptions.isShowDetailModal)"
+                                class="h-5 w-5 text-gray-400 hover:text-gray-600" />
                             <PencilSquareIcon class="h-5 w-5 text-gray-400 hover:text-gray-600" />
                             <TrashIcon class="h-5 w-5 text-gray-400 hover:text-gray-600" />
                         </div>
@@ -77,6 +78,7 @@
 <script setup>
 import Breadcrumb from '@/components/ui/Breadcrumb.vue';
 import AddEntityModal from '@/components/modal/AddEntityModal.vue';
+import DetailEntityModal from '@/components/modal/DetailEntityModal.vue';
 import Button from '@/components/ui/Button.vue';
 import { MagnifyingGlassIcon, ArrowsUpDownIcon, PencilSquareIcon, TrashIcon, EyeIcon } from '@heroicons/vue/24/solid'
 
@@ -113,15 +115,13 @@ const lengths = [
 ]
 
 
-function closeAddModal() {
-    modalOptions.value
-        .isShowAddModal = false
-}
-function showAddModal() {
-    modalOptions.value
-        .isShowAddModal = true
+const toggleModal = (modelName, state) => {
+    modalOptions.value[modelName] = state
 }
 
-
+const selected = {
+    name: 'Apple MacBook Pro 17',
+    description: 'Electronics is a scientific and engineering discipline that studies and applies the principles of physics to design, create, and operate devices.'
+}
 
 </script>
