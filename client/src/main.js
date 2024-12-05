63e5324fd761c3a5996ea300c07a9de21ba88a09
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-12-05 22:37:43 +0300

    dashboard bar chart added with dumy data

--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,12 @@ import './style.css'
 import App from './App.vue'
 import router from '@/router/route'
 import axios from 'axios'
+import VueApexCharts from "vue3-apexcharts";
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 
 createApp(App)
+    .use(VueApexCharts)
     .use(createPinia())
     .use(router)
     .mount('#app')
--- client/src/pages/Dashboard.vue
+++ client/src/pages/Dashboard.vue
@@ -7,6 +7,13 @@
     <!--Trends and Visual Insights-->
     <section class="mt-10">
         <p class="text-gray-600 my-3">Trends and Visual Insights</p>
+        <div class="">
+            <!--Bar Chart-->
+            <div class=" w-full rounded-lg shadow">
+                <p class="p-6 text-gray-400">Top Categories</p>
+                <apexchart width="100%" height="400" type="bar" :options="options" :series="series"></apexchart>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -45,4 +52,65 @@ const dashboards = computed(() => [
         number: dashboardStore.getDashboard?.userCount || 0,
     },
 ]);
+
+var series = [{
+    name: 'Sold Products',
+    data: [44, 55, 41, 67, 22, 22]
+}]
+
+var options = {
+    plotOptions: {
+        bar: {
+            borderRadius: 10,
+            columnWidth: '50%',
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        width: 0
+    },
+    grid: {
+        row: {
+            colors: ['#fff', '#f2f2f2']
+        }
+    },
+    xaxis: {
+        labels: {
+            rotate: -45
+        },
+        categories: [
+            "Electronics",
+            "Furniture",
+            "Clothing",
+            "Groceries",
+            "Health & Beauty",
+            "Automotive",
+        ],
+
+    },
+    yaxis: {
+        title: {
+            text: 'Best categories of this month',
+        },
+    },
+    colors: ['#312E81'],
+    fill: {
+        type: 'gradient',
+        gradient: {
+            shade: 'light',
+            type: "horizontal",
+            shadeIntensity: 0.25,
+            gradientToColors: undefined,
+            inverseColors: true,
+            opacityFrom: 0.85,
+            opacityTo: 0.85,
+            stops: [50, 0, 100]
+        },
+    }
+};
+
+
+
 </script>
