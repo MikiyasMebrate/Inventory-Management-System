b4136b2a6e4ec5d790bc85385575322e84f1fa34
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-12-06 10:48:56 +0300

    notification preference form created

--- client/src/pages/NotificationPreference.vue
+++ client/src/pages/NotificationPreference.vue
@@ -6,10 +6,10 @@
         <form>
             <ul
                 class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                <BorderedCheckBox label="Sale" />
-                <BorderedCheckBox label="Restock" />
-                <BorderedCheckBox label="Return" />
-                <BorderedCheckBox label="Category" />
+                <BorderedCheckBox v-model="formData.receiveSaleNotifications" label="Sale" />
+                <BorderedCheckBox v-model="formData.receiveRestockNotifications" label="Restock" />
+                <BorderedCheckBox v-model="formData.receiveReturnNotifications" label="Return" />
+                <BorderedCheckBox v-model="formData.receiveCategoryNotifications" label="Category" />
             </ul>
             <div class="flex justify-end mt-5">
                 <Button title="Update Preference"></Button>
@@ -22,4 +22,15 @@
 import Breadcrumb from '@/components/ui/Breadcrumb.vue';
 import BorderedCheckBox from '@/components/ui/BorderedCheckBox.vue';
 import Button from '@/components/ui/Button.vue';
+import { ref } from 'vue';
+
+
+
+const formData = ref({
+    receiveSaleNotifications: false,
+    receiveRestockNotifications: false,
+    receiveReturnNotifications: false,
+    receiveCategoryNotifications: false,
+    receiveProductNotifications: false
+})
 </script>
