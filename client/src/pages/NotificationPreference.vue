7c19412bb258e80b3d6a7c3dc619c676f34a59f8
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-12-06 10:57:28 +0300

    user notification preference fetchedd

--- client/src/pages/NotificationPreference.vue
+++ client/src/pages/NotificationPreference.vue
@@ -3,7 +3,7 @@
 
     <section class="mt-5">
         <p class="my-5 text-3xl font-bold">Preferences</p>
-        <form>
+        <form @submit.prevent="onSubmit">
             <ul
                 class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                 <BorderedCheckBox v-model="formData.receiveSaleNotifications" label="Sale" />
@@ -12,7 +12,7 @@
                 <BorderedCheckBox v-model="formData.receiveCategoryNotifications" label="Category" />
             </ul>
             <div class="flex justify-end mt-5">
-                <Button title="Update Preference"></Button>
+                <Button type="submit" title="Update Preference"></Button>
             </div>
         </form>
     </section>
@@ -23,8 +23,10 @@ import Breadcrumb from '@/components/ui/Breadcrumb.vue';
 import BorderedCheckBox from '@/components/ui/BorderedCheckBox.vue';
 import Button from '@/components/ui/Button.vue';
 import { ref } from 'vue';
+import { onMounted } from 'vue';
+import { useNotificationPreferenceStore } from '@/store/notificationPreference';
 
-
+const preferenceStore = useNotificationPreferenceStore()
 
 const formData = ref({
     receiveSaleNotifications: false,
@@ -33,4 +35,18 @@ const formData = ref({
     receiveCategoryNotifications: false,
     receiveProductNotifications: false
 })
+
+
+onMounted(() => {
+    preferenceStore.fetchPreferences()
+
+    formData.value.receiveSaleNotifications = preferenceStore.preferences.receiveSaleNotifications
+    formData.value.receiveRestockNotifications = preferenceStore.preferences.receiveRestockNotifications
+    formData.value.receiveReturnNotifications = preferenceStore.preferences.receiveReturnNotifications
+    formData.value.receiveCategoryNotifications = preferenceStore.preferences.receiveCategoryNotifications
+})
+
+const onSubmit = () => {
+    console.log("form submitted")
+}
 </script>
